2985c476eb3ff4cb625abc20affbfb5080bcead8
Serikat Newbie Indonesia <162957536+serikatnewbie@users.noreply.github.com>
2025-04-06 11:43:52 +0000

    hr edit

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,7 @@ import { SITE } from "@/config";
 export const SOCIALS = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/serikatnewbie",
     linkTitle: ` ${SITE.title} on Github`,
     icon: IconGitHub,
   },
@@ -23,13 +23,13 @@ export const SOCIALS = [
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/company/serikatnewbie-id",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:serikatnewbie.id@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
